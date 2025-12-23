28c6f240f185dd4f0d9192b7a4284732d2099f5f
rebanawa10-lab <rebanawa10@gmail.com>
2025-12-23 15:33:17 +0800

    Initial Node.js app

--- environment.ts
+++ environment.ts
@@ -2,3 +2,8 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:5000/api'
 };
+
+//  http://localhost:5000
+//  http://localhost:5000/
+//  http://localhost:5000/api/projects
+//  http://localhost:5000/api/sales
